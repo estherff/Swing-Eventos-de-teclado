aa62bcdb552188f739b8148cf913c8651e286280
estherff <estherff.edu@gmail.com>
2021-05-10 09:09:04 +0200

    V1- Lectura de un area de texto y eventos que encían las vocales a otra área de texto.

---
+++ src/main/java/gal/teis/swing/eventos/teclado/Principal.java
@@ -0,0 +1,24 @@
+package gal.teis.swing.eventos.teclado;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Esther Ferreiro
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       VentanaFrame miVentana = new VentanaFrame();
+       miVentana.setVisible(true);
+    }
+    
+}
